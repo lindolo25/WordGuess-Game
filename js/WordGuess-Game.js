154330a58f4147d98a11538d8ab9de894c4a7bb6
lindolo25 <lindolo25@outlook.com>
2018-09-01 22:59:36 -0400

    implemente the keypressed functionallity

--- js/WordGuess-Game.js
+++ js/WordGuess-Game.js
@@ -15,8 +15,7 @@
 
 const wordCollection = ["Uno", "Dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "dies"];
 var currentWord = "";
-var pressedKeys = "so";
-var sExpression = new RegExp("[^" + pressedKeys + "]","gi");
+var pressedKeys = "";
 const totalLettersAttents = 10;
 var remainingLettersAttents = totalLettersAttents;
 var wins = 0
@@ -29,9 +28,7 @@ function init()
     currentWord = wordCollection[rand];
     console.log(currentWord);
 
-    // set the word on screen
-    var secretWordEle = document.getElementById("secret-word");
-    secretWordEle.innerHTML = currentWord.replace(sExpression, "_");
+    printOnScreen();
 }
 
 function resetGame()
@@ -39,7 +36,37 @@ function resetGame()
 
 }
 
-function keyPressed(key)
+function keyPressed(input)
 {
+    pressedKeys += input.key;
+    remainingLettersAttents--;
+    printOnScreen();
+    
+    var sExpression = new RegExp("[^" + pressedKeys + "]","gi");
+    var temp = currentWord.replace(sExpression, "_");
+    if(currentWord === temp)
+    {
+        this.wins++;
+        alert("You Win!");
+    }
+    else if(remainingLettersAttents <= 0)
+    {
+        alert("You lose!");
+    }
+}
 
+function printOnScreen()
+{
+    // set the word on screen
+    var element = document.getElementById("secret-word");
+    var sExpression = new RegExp("[^" + pressedKeys + "]","gi");
+    element.innerHTML = currentWord.replace(sExpression, "_");
+
+    // set remaining guesses
+    var element = document.getElementById("remaining-guesses");
+    element.innerHTML = remainingLettersAttents;
+
+    // set guessed letters
+    var element = document.getElementById("guessed-letters");
+    element.innerHTML = pressedKeys.toUpperCase();
 }
