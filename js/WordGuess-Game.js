c98d4f569f7ad3332e3d2c112625a9d1d1fd7f3f
lindolo25 <lindolo25@outlook.com>
2018-09-02 23:17:27 -0400

    fixed random so it does not repeat. added a game over when random time out.

--- js/WordGuess-Game.js
+++ js/WordGuess-Game.js
@@ -1,20 +1,98 @@
 
-const wordCollection = ["Uno", "Dos 2", "tres", "cuatro", "cinco cincuenta", "seis", "siete", "ocho", "nueve", "dies"];
+const wordCollection = [
+    {
+        title : "007",
+        audioSource : "media/007.ogg"
+    },
+    {
+        title : "A Space Odyssey",
+        audioSource : "media/aSpaceOdyssey.ogg"
+    },
+    {
+        title : "The Goodfather",
+        audioSource : "media/theGoodfather.ogg"
+    },
+    {
+        title : "Jaws",
+        audioSource : "media/jaws.ogg"
+    },
+    {
+        title : "Star Wars",
+        audioSource : "media/starWars.ogg"
+    },
+    {
+        title : "Superman",
+        audioSource : "media/superman.ogg"
+    },
+    {
+        title : "Indiana Jones",
+        audioSource : "media/IndianaJones.ogg"
+    },
+    {
+        title : "E.T.",
+        audioSource : "media/et.ogg"
+    },
+    {
+        title : "Jurassic Park",
+        audioSource : "media/jurassicPark.ogg"
+    },
+    {
+        title : "Schindler's List",
+        audioSource : "media/schindlersList.ogg"
+    },
+    {
+        title : "Forrest Gump",
+        audioSource : "media/forrestGump.ogg"
+    },
+    {
+        title : "Speed",
+        audioSource : "media/speed.ogg"
+    },
+    {
+        title : "Braveheart",
+        audioSource : "media/braveheart.ogg"
+    },
+    {
+        title : "Titanic",
+        audioSource : "media/titanic.ogg"
+    },
+    {
+        title : "Gladiator",
+        audioSource : "media/gladiator.ogg"
+    }     
+];
+
+
+
 const allowedLetters = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
 const totalLettersAttempts = 10;
 
 var currentWord = "";
-var guessedLetters = " ";
+var guessedLetters = " .'";
 var remainingGuessAttempts = totalLettersAttempts;
 var wins = 0;
+var playedWords = "";
 
 
 function init()
 {
     // choose a ramdom Word
-    var rand =  Math.floor((Math.random() * 10));
-    currentWord = wordCollection[rand];
+    var rand =  nonRepeatRandom(wordCollection.length);
+    if(rand === undefined) {
+        gameOver();
+        return;
+    }
+    currentWord = wordCollection[rand].title;
     console.log(currentWord);
+
+    // set the track on the audio object
+    var a = document.getElementById("oggSource");
+    a.setAttribute("src", wordCollection[rand].audioSource);
+    
+    // play the track
+    var player = document.getElementById('player');
+    player.load(); 
+    player.play();
 
     printOnScreen();
 }
@@ -24,7 +102,6 @@ function keyPressed(input)
     if(allowedLetters.indexOf(input.key.toUpperCase()) >= 0 && guessedLetters.indexOf(input.key.toUpperCase()) < 0 && remainingGuessAttempts > 0)
     {
         guessedLetters += input.key.toUpperCase();
-        console.log(guessedLetters);
         remainingGuessAttempts--;
         printOnScreen();
         
@@ -46,9 +123,15 @@ function keyPressed(input)
 
 function resetGame()
 {
-    guessedLetters = " ";
+    guessedLetters = " .'";
     remainingGuessAttempts = totalLettersAttempts;
     init();
+}
+
+function gameOver()
+{
+    document.onkeydown = undefined;
+    alert("Game Over");
 }
 
 function printOnScreen()
@@ -68,5 +151,21 @@ function printOnScreen()
 
     // set guessed letters
     var element = document.getElementById("guessed-letters");
-    element.innerHTML = guessedLetters;
+    element.innerHTML = guessedLetters.substring(3,guessedLetters.length);
+}
+
+function nonRepeatRandom(max, calls = 0)
+{
+    if( calls++ > 10000 ) return undefined;
+
+    var selected = Math.floor((Math.random() * max));
+    if(playedWords.indexOf(selected) < 0)
+    {
+        playedWords += selected;
+        return selected;
+    }
+    else
+    {
+        return nonRepeatRandom(max, calls);
+    }
 }
